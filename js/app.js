3c259fff47e6daf64ec9eb139f32d27b92647c62
MonicaB <mlbdenver@gmail.com>
2014-06-08 19:49:55 -0600

    Turning it in now
    Still lots of ideas, but good enough for now.

--- js/app.js
+++ js/app.js
@@ -9,20 +9,23 @@ $(document).ready(function() {
             answers:["Chocolate", "Vanilla", "Pistacio", "Strawberry"],
             correct: 1,
             corrAns:"Vanilla is the most popular flavor of ice cream in the US, accounting for 20% to 29% of all ice cream purchased!",
+            trivia:"Did you know that the vanilla bean is the fruit of a tropical American species of orchid???",
         },
         {
             number: "Question #2",
             quest: "We know Americans love ice cream. But which country consumes the second most ice cream per capital?",
             answers:["Canada", "England", "New Zealand", "Sweden"],
             correct: 2,
-            corrAns:"New Zealanders love there ice cream too!",
+            corrAns:"New Zealanders love their ice cream too!",
+            trivia:"New Zealanders consume 23 liters of ice cream per person each year! (That's 6 gallons for us imperial system users).",
         },
         {
             number: "Question #3",
             quest: "Which of the following is not a Ben & Jerry's Ice Cream flavor?",
             answers:["Karamel Sutra", "Chubby Hubby", "Half Baked", "Sherbert Holmes"],
             correct: 3,
-            corrAns:"Sherbery Holmes is not a real Ben & Jerry's Flavor.",
+            corrAns:"Sherbery Holmes is the Ben & Jerry's imposter Flavor!",
+            trivia:"But Sherbert Holmes does sound good!",
         },
         {
             number: "Question #4",
@@ -30,6 +33,7 @@ $(document).ready(function() {
             answers:["2 million", "50 million", "350 million", "800 million"],
             correct: 3,
             corrAns:"The US produces 800 million gallons of ice cream each year.",
+            trivia:"800 million gallons is a lot of ice cream!",
         },
 
         {
@@ -38,6 +42,7 @@ $(document).ready(function() {
             answers:["Sunday", "Wednesday", "Friday", "Saturday"],
             correct: 0,
             corrAns:"Sunday is the day for ice cream.",
+            trivia:"But ice cream is awesome every day!",
         }
     ];
  
@@ -64,11 +69,15 @@ $(document).ready(function() {
         $("#new").hide();
         $("#questNum").hide();
         $("#box ul").hide();
+        $("#begin").show();
+        $("#corrAns").hide();
     }
     /*--- Reset Quiz on Restart Button Click---*/
     $("#new").click(function() {
         console.log("Quiz reset");
         restartQuiz();
+        $("#questText").text("Ready to play again?");
+        
     });
 
     /*--- Start Quiz on button click---*/
@@ -79,6 +88,7 @@ $(document).ready(function() {
         $("#new").show();
         $("#questNum").show();
         $("#box ul").show();
+         $("#score").hide();
         questNum = questAns+1;
         askQuest();
     });
@@ -93,14 +103,18 @@ $(document).ready(function() {
             $("#3rdChoice").text(question[questAns].answers[2]);
             $("#4thChoice").text(question[questAns].answers[3]);
             $("#questNum").text(question[questAns].number);
-            $("#score").show();
             $("#corrAns").hide();
             $("#submit").show();
             $("#next").hide();
             $(":radio").click(function() {
                 $("#submit").prop("disabled",false);
             });
-
+            if (questAns === 0) {
+                $("#score").hide();            
+            }
+            else {
+                $("#score").show();
+            }
     };
        
     
@@ -110,11 +124,12 @@ $(document).ready(function() {
     $("body").on('click', '#submit', function() {
         var userGuess = $("input[type='radio']:checked").val();
         $("#submit").hide();
+        $("#score").show();
         if (userGuess == question[questAns].correct) {
             console.log("userGuess is "+ userGuess);
             console.log("correct");
             $("#corrAns").show();
-            $("#corrAns").text("CORRECT! ");
+            $("#corrAns").text("CORRECT! " + question[questAns].trivia);
             correctAns ++;
             questAns++;
             if(questAns === 5) {
